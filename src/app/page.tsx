e89f87f7ab0bd21ed740f196c7795b6951c35a94
p11 <github@programmersunion.in>
2025-06-25 18:47:03 +0530

    updated footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ import healthData from '../../public/data/health.json';
 
 export default function Home() {
   return (
-    <div className="container width-first">
+    <div className="container">
       <NewsArticleComponent mainArticle={politicsData[0]} />
 
 
